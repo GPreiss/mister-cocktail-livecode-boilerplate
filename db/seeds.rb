982ca7647b3ffc065f8d31f36fb50e4f82a83aef
Gabriela Preiss <gabriela.preiss@gmail.com>
2018-01-03 15:15:06 +0000

    adding dose model w/ associations and validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -88,7 +88,7 @@ cocktails = [
   }
 ]
 
-ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
+ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rum)
 ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
 
 
